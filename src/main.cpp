e62ebb60812298f7e8e4c23f284e3d9e9b0dca66
Shreyas Manjunath <shreyas.manjunath@node-robotics.com>
2025-08-21 00:55:25 +0200

    feat: add estop fuctionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 // Copyright 2025 Shreyas Manjunath
 
 #include <memory>
+#include <rclcpp/executors/multi_threaded_executor.hpp>
 
 #include "bin_picking_mockup/barcode_publisher.hpp"
+#include "bin_picking_mockup/estop_handler.hpp"
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<bin_picking_mockup::BarcodePublisher>());
+
+  auto barcode_publisher =
+      std::make_shared<bin_picking_mockup::BarcodePublisher>();
+  auto estop_handler = std::make_shared<bin_picking_mockup::EStopHandler>();
+  rclcpp::executors::MultiThreadedExecutor executor;
+
+  executor.add_node(barcode_publisher);
+  executor.add_node(estop_handler);
+  executor.spin();
   rclcpp::shutdown();
   return 0;
 }
