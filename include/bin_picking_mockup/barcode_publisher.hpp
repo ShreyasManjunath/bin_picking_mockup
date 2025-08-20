dc26e95744258a6a3cdfbcfa8137f9a8f71c46ae
Shreyas Manjunath <shreyas.manjunath@node-robotics.com>
2025-08-21 00:02:46 +0200

    feat(bin_picking): add get_barcode service

--- include/bin_picking_mockup/barcode_publisher.hpp
+++ include/bin_picking_mockup/barcode_publisher.hpp
@@ -3,23 +3,32 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp/service.hpp>
 #include <std_msgs/msg/int32.hpp>
 #include <vector>
 
+#include "bin_picking_mockup/srv/get_barcode.hpp"
 namespace bin_picking_mockup {
 
 class BarcodePublisher : public rclcpp::Node {
  public:
   BarcodePublisher();
   ~BarcodePublisher() override = default;
-  void publish_barcode();
+  auto publish_barcode() -> void;
+  auto handle_get_barcode(
+      const std::shared_ptr<bin_picking_mockup::srv::GetBarcode::Request>
+      /*request*/,
+      std::shared_ptr<bin_picking_mockup::srv::GetBarcode::Response> response)
+      -> void;
 
  private:
   rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr barcode_pub_;
-  std::vector<int16_t> barcodes = {123, 456, 789, 987, 654};
-  int16_t last_barcode;
+  int32_t last_barcode;
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Service<bin_picking_mockup::srv::GetBarcode>::SharedPtr
+      service_handle_;
 };
 
 }  // namespace bin_picking_mockup
--- src/barcode_publisher.cpp
+++ src/barcode_publisher.cpp
@@ -2,7 +2,11 @@
 
 #include "bin_picking_mockup/barcode_publisher.hpp"
 
+#include <cstdint>
+#include <memory>
 #include <random>
+
+#include "bin_picking_mockup/srv/get_barcode.hpp"
 
 namespace bin_picking_mockup {
 
@@ -13,9 +17,12 @@ BarcodePublisher::BarcodePublisher()
   timer_ = this->create_wall_timer(
       std::chrono::seconds(1),
       std::bind(&BarcodePublisher::publish_barcode, this));
+  service_handle_ = this->create_service<bin_picking_mockup::srv::GetBarcode>(
+      "get_barcode", std::bind(&BarcodePublisher::handle_get_barcode, this,
+                               std::placeholders::_1, std::placeholders::_2));
 }
 
-void BarcodePublisher::publish_barcode() {
+auto BarcodePublisher::publish_barcode() -> void {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<> dist(100, 999);
@@ -23,6 +30,14 @@ void BarcodePublisher::publish_barcode() {
   msg.data = dist(gen);
   barcode_pub_->publish(msg);
   last_barcode = msg.data;
+}
+
+auto BarcodePublisher::handle_get_barcode(
+    const std::shared_ptr<
+        bin_picking_mockup::srv::GetBarcode::Request> /*request*/,
+    std::shared_ptr<bin_picking_mockup::srv::GetBarcode::Response> response)
+    -> void {
+  response->barcode = static_cast<int32_t>(last_barcode);
 }
 
 }  // namespace bin_picking_mockup
